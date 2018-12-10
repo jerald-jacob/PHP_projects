214023d2e40cda1900e6a664b84acb7cad9f434d
Jerald Jacob <jeraldjacob10@gmail.com>
2018-12-10 08:43:24 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,184 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<!-- Head -->
+<head>
+<title>Metal Panel an Exterior Category Bootstrap responsive Website Template | Home :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<meta name="keywords" content="Metal Panel a Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+
+<!-- Bootstrap-CSS -->	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" media="all">
+<!-- Fontawesome-CSS --><link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all" />	
+<!-- Nav-CSS -->	
+<link href="css/nav.css" rel="stylesheet" type="text/css" media="all" />
+ <script src="js/modernizr.custom.js"></script>
+<!-- //Nav-CSS -->	
+<link rel="stylesheet" type="text/css" href="css/style.css" /><!-- style-CSS -->
+<!-- fonts -->
+	<link href="//fonts.googleapis.com/css?family=Josefin+Sans:100,100i,300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+<!-- fonts -->
+<!-- Default-JavaScript-File -->
+	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- //Default-JavaScript-File -->
+<!-- scrolling script -->
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- //scrolling script -->
+</head>
+<!-- Head -->
+<body>
+<!-- logo section -->
+<div class="logo">
+	<a href="index.html"><h1>Metal  Panel</h1></a>
+</div>
+<!-- /logo section -->
+<!-- navigation section -->
+<div>
+	<p>
+		<button id="trigger-overlay" type="button">
+			<i class="fa fa-bars" aria-hidden="true"></i>
+		</button>
+	</p>
+</div>
+	<!-- open/close -->
+		<div class="overlay overlay-hugeinc">
+			<button type="button" class="overlay-close">Close</button>
+			<nav>
+				<ul>
+					<li><a href="teacherlogin/">Teacher Login</a></li>
+					<li><a href="student_login/">Student Login</a></li>
+					<li><a href="admin_panel/login/">Admin Panel</a></li>
+					
+				</ul>
+			</nav>
+		</div>
+	<!-- /open/close -->
+<!-- /navigation section -->
+
+<!-- banner section -->	 
+<section class="banner" id="home"> 
+	<!-- Slider -->
+		<div class="slider">
+			<ul class="rslides" id="slider">
+				<li class="first-slide w3ls">
+					<img src="images/Harvard.jpg" alt="Couch Hunter">
+					<h3>A thorough knowledge of the Bible is worth more than a college education.</h3>
+			
+					
+				</li>
+				<li class="second-slide aits">
+					<img src="images/Harvard-PP-course-by-Tal-Ben-Shahar.jpg" alt="Couch Hunter">
+					<h3>A thorough knowledge of the Bible is worth more than a college education.</h3>
+				</li>
+				<li class="third-slide w3-agileits">
+					<img src="images/4_main.jpg" alt="Couch Hunter">
+					<h3>A thorough knowledge of the Bible is worth more than a college education.</h3>
+					
+				</li>
+			</ul>
+		</div>
+	<!-- //Slider -->
+</section>	
+<!-- /banner section -->
+<!-- /banner bottom -->
+
+<!-- //banner bottom -->
+	<!-- /services -->
+	
+
+	<!-- //services -->
+
+<!-- news -->
+
+<!-- //news -->
+<!-- testimonials -->
+	<div class="testimonials">
+		<div class="container">
+			<h3 class="heading-agileinfo">Developed By Akhil</h3>
+				
+		<div class="w3_testimonials_grids w3_testimonials_grids">
+			 	
+		</div>
+	</div>
+</div>
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+	<!-- //here ends scrolling icon -->
+<!-- js for smooth scrollings -->
+	<script src="js/SmoothScroll.min.js"></script>
+<!-- js for smooth scrollings -->
+
+<!-- Resopnsive-Slider-JavaScript -->
+			<script src="js/responsiveslides.min.js"></script>
+			<script>
+				$(function () {
+					$("#slider").responsiveSlides({
+						auto: true,
+						nav: true,
+						speed: 2000,
+						namespace: "callbacks",
+						pager: true,
+					});
+				});
+			</script>
+<!-- //Resopnsive-Slider-JavaScript -->
+
+	<!-- for nav menu-js-file-->
+		<script src="js/classie.js"></script>
+		<script src="js/demo1.js"></script>
+	<!-- for nav menu-js-file-->
+<!-- for testimonials slider-js-script-->
+<!-- testimonials -->
+	<!-- required-js-files-->
+							<link href="css/owl.carousel.css" rel="stylesheet">
+							    <script src="js/owl.carousel.js"></script>
+							        <script>
+							    $(document).ready(function() {
+							      $("#owl-demo").owlCarousel({
+							        items : 1,
+							        lazyLoad : true,
+							        autoPlay : false,
+							        navigation : true,
+							        navigationText : true,
+							        pagination : true,
+							      });
+							    });
+							    </script>
+								 <!--//required-js-files-->
+
+<!-- for testimonials slider-js-script-->
+<!-- move to top-js-files -->
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+<!-- //move to top-js-files -->
+
+<!-- Default-JavaScript-File -->
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<!-- //Default-JavaScript-File -->
+</body>
+<!-- //body -->
+</html>
+<!-- html -->
